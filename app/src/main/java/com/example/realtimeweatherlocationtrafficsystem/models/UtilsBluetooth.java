9494d92125768173c67ae17f43d0b95d7b14ed97
SILITRA Mihai-Andrei <silitramihai1999@gmail.com>
2020-08-14 16:51:49 +0300

    resolved bug: on back pressed freeze activity. current location implemented

--- app/src/main/java/com/example/realtimeweatherlocationtrafficsystem/models/UtilsBluetooth.java
+++ app/src/main/java/com/example/realtimeweatherlocationtrafficsystem/models/UtilsBluetooth.java
@@ -42,7 +42,7 @@ public class UtilsBluetooth {
     public final static int UNKNOWN_COMMAND = 24;
     public final static String STATES_STRING =
             "E"+BLUETOOTH_RECEIVE_DELIMITER+
-            "G"+BLUETOOTH_RECEIVE_DELIMITER+
+            "G"+BLUETOOTH_RECEIVE_STATE_DELIMITER+
             "g"+BLUETOOTH_RECEIVE_STATE_DELIMITER+
             "D"+BLUETOOTH_RECEIVE_STATE_DELIMITER+
             "t"+BLUETOOTH_RECEIVE_STATE_DELIMITER+
@@ -55,23 +55,22 @@ public class UtilsBluetooth {
             "a"+BLUETOOTH_RECEIVE_STATE_DELIMITER+
             "u"+BLUETOOTH_RECEIVE_STATE_DELIMITER;
 
-    public static String[] getReceivedMessage(String currentTextBox, String message, Context context) {
-        if (currentTextBox == null || message == null) return null;
+    public static String getReceivedMessage(String currentTextBox, String message, Context context) {
+        if (message == null) return null;
         /* first string is the message which is displayed
          * second string is the action which must be treated; null means no action * */
-        String[] result = new String[2];
-        result[1] = ""; //no action needed
+        String result = "";
         String time = "<" + Utils.getTime() + "> ";
         if (message.contains(STATE_START)) {
-            result[0] = "RTWL System started\n";
+            result = "RTWL System started\n";
         }
         else if(message.contains(STATE_PING)){
-            result[0] = "Arduino is active.\n";
+            result = "Arduino is active.\n";
         }
         else if(message.contains(".")){
             String[] string = message.split(" ");
             if(string.length==6){
-                result[0] = string[0] + " "+string[1]
+                result = string[0] + " "+string[1]
                         + "\n\t-\t" + UtilsGoogleMaps.getWeatherString(UtilsGoogleMaps.getWeatherStringIndex(Integer.parseInt(string[2])), context)
                         + "\n\t-\tTemperature: " + string[3]
                         + "\n\t-\tHumidity: "+string[4]
@@ -79,48 +78,48 @@ public class UtilsBluetooth {
             }
         }
         else if(message.contains(STATES_STRING.substring(STATE_ERROR, STATE_ERROR+2))){
-            result[0] = "An error occurred to Arduino microprocessor\n";
+            result = "An error occurred to Arduino microprocessor\n";
         }
         else if(message.contains(STATES_STRING.substring(STATE_GPS_NOT_WORKING, STATE_GPS_NOT_WORKING+2))){
-            result[0] = "GPS is not working!\n";
+            result = "GPS is not working!\n";
         }
         else if(message.contains(STATES_STRING.substring(STATE_GPS_INVALID_DATA, STATE_GPS_INVALID_DATA+2))){
-            result[0] = "Invalid data from GPS\n";
+            result = "Invalid data from GPS\n";
         }
         else if(message.contains(STATES_STRING.substring(STATE_TEMP_HUM_SENSOR_NOT_WORKING, STATE_TEMP_HUM_SENSOR_NOT_WORKING+2))){
-            result[0] = "Temperature and humidity sensor is not working!\n";
+            result = "Temperature and humidity sensor is not working!\n";
         }
         else if(message.contains(STATES_STRING.substring(STATE_TEMP_INVALID_DATA, STATE_TEMP_INVALID_DATA+2))){
-            result[0] = "Invalid temperature value!\n";
+            result = "Invalid temperature value! Received value: " + message.split(":")[1];
         }
         else if(message.contains(STATES_STRING.substring(STATE_HUM_INVALID_DATA, STATE_HUM_INVALID_DATA+2))){
-            result[0] = "Invalid humidity value!\n";
+            result = "Invalid humidity value! Received value: " + message.split(":")[1];
         }
         else if(message.contains(STATES_STRING.substring(STATE_PROXY_SENSOR_NOT_WORKING, STATE_PROXY_SENSOR_NOT_WORKING+2))){
-            result[0] = "Proxy sensor is not working!\n";
+            result = "Proxy sensor is not working!\n";
         }
         else if(message.contains(STATES_STRING.substring(STATE_WIND_SENSOR_NOT_WORKING, STATE_WIND_SENSOR_NOT_WORKING+2))){
-            result[0] = "Wind sensor not working!\n";
+            result = "Wind sensor not working!\n";
         }
         else if(message.contains(STATES_STRING.substring(STATE_WIND_SENSOR_INVALID_DATA, STATE_WIND_SENSOR_INVALID_DATA+2))){
-            result[0] = "Invalid wind sensor value!\n";
+            result = "Invalid wind sensor value! Received value: " + message.split(":")[1];
         }
         else if(message.contains(STATES_STRING.substring(STATE_WEATHER_CODE_INVALID, STATE_WEATHER_CODE_INVALID+2))){
-            result[0] = "Invalid weather code value\n";
+            result = "Invalid weather code value Received value: " + message.split(":")[1];
         }
         else if(message.contains(STATES_STRING.substring(STATE_AIR_SENSOR_NOT_WORKING, STATE_AIR_SENSOR_NOT_WORKING+2))){
-            result[0] = "Air quality sensor is not working\n";
+            result = "Air quality sensor is not working\n";
         }
         else if(message.contains(STATES_STRING.substring(STATE_AIR_SENSOR_INVALID_DATA, STATE_AIR_SENSOR_INVALID_DATA+2))){
-            result[0] = "Invalid air quality sensor value\n";
+            result = "Invalid air quality sensor value Received value: " + message.split(":")[1];
         }
         else if(message.contains(STATES_STRING.substring(UNKNOWN_COMMAND, UNKNOWN_COMMAND+2))){
-            result[0] = "Unknown command!\n";
+            result = "Unknown command!\n";
         }
-        else result[0] = "";
-        result[0] = time + result[0];
-        if(message.length() + currentTextBox.length() <= Utils.MAX_RECEIVE_BOX_LENGTH) {
-            result[0] = currentTextBox + result[0];
+        else result = null;
+        result = time + result;
+        if(currentTextBox!=null && message.length() + currentTextBox.length() <= Utils.MAX_RECEIVE_BOX_LENGTH) {
+            result = currentTextBox + result;
         }
         return result;
     }
